938c05bd6636d0a50f8f7c05ad35fe46224f56b6
Viktor Churikov <scanlave@gmail.com>
2021-08-19 23:40:55 +0800

    Finish

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trafficLightRed: UIView!
+    @IBOutlet var trafficLightYellow: UIView!
+    @IBOutlet var trafficLightGreen: UIView!
+    @IBOutlet var startButton: UIButton!
+    var valueSet = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        trafficLightRed.layer.cornerRadius = trafficLightRed.frame.size.width/2
+        trafficLightYellow.layer.cornerRadius = trafficLightYellow.frame.size.width/2
+        trafficLightGreen.layer.cornerRadius = trafficLightGreen.frame.size.width/2
+        
+        trafficLightRed.alpha = 0.2
+        trafficLightYellow.alpha = 0.2
+        trafficLightGreen.alpha = 0.2
     }
-
+    @IBAction func startButtonPressed() {
+        valueSet += 1
+        switch valueSet {
+            case 1:
+                trafficLightRed.alpha = 1
+                startButton.setTitle("next", for: .normal)
+            case 2:
+                trafficLightRed.alpha = 0.2
+                trafficLightYellow.alpha = 1
+            case 3:
+                trafficLightYellow.alpha = 0.2
+                trafficLightGreen.alpha = 1
+                startButton.setTitle("finish", for: .normal)
+                valueSet = -1
+            default:
+                trafficLightRed.alpha = 0.2
+                trafficLightYellow.alpha = 0.2
+                trafficLightGreen.alpha = 0.2
+                startButton.setTitle("start", for: .normal)
+                valueSet = 0
+        }
+    }
+    
+        
+    
 
 }
 
